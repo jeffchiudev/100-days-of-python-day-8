765b6540342b06d7d45e633917e9f88f05723be0
Jeff Chiu <jwchiu82@gmail.com>
2021-06-06 20:47:21 -0700

    playing with arguments and parameters in a function declaration

---
+++ day-8-practice.py
@@ -0,0 +1,12 @@
+# def greet():
+#     print("hello")
+#     print("Goodbye")
+#     print("you're still here?")
+
+# greet()
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Jeff")
