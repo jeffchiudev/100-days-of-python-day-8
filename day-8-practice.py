bf14ea7928ab9f7b42e7fd79fefa7345ed4b3a46
Jeff Chiu <jwchiu82@gmail.com>
2021-06-06 20:54:42 -0700

    demonstrate keyword vs positional arguments

--- day-8-practice.py
+++ day-8-practice.py
@@ -5,8 +5,14 @@
 
 # greet()
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}")
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}")
 
-greet_with_name("Jeff")
+# greet_with_name("Jeff")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with(location="Portland", name="Jeff" )
